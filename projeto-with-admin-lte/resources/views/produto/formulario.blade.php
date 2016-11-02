78ed342ea8f0bb4c953429103ed8477c9a5b5104
Vinicius Sarmento Costa Siqueira <vinicius.eng.comp92@gmail.com>
2016-11-02 11:07:19 -0200

    Alterando posicionamento do formulário

--- projeto-with-admin-lte/resources/views/produto/formulario.blade.php
+++ projeto-with-admin-lte/resources/views/produto/formulario.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('conteudo')
-<form action="/produtos/adiciona" method="POST">
+<form action="/produtos/adiciona" class="form-horizontal" method="POST">
 
     <input type="hidden" name="_token" value="{{csrf_token()}}">
 
     <div class="form-group">
         <div class="col-md-6">
-            <label >Nome</label>
+            <label  >Nome</label>
             <input name="nome" class="form-control" required>
         </div>
         <div class="col-md-6">
@@ -19,6 +19,8 @@
                 @endforeach
             </select>
         </div>
+    </div>
+    <div class="form-group">
         <div class="col-md-6">
             <label >Valor</label>        
             <input name="valor" class="form-control" required>  
@@ -27,19 +29,24 @@
             <label>Quantidade</label>
             <input name="quantidade" class="form-control col-md-3" required>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-12">
             <label>Descrição</label>
-            <textarea name="descricao" class="form-control" rows="3" required></textarea> 
+            <textarea name="descricao" class="form-control" rows="3" style="resize: none;" required></textarea> 
         </div>
-
     </div>
-    
-    <div class="form-group">
 
-        <div class="col-md-6">
-            <button class="btn btn-success" type="submit">Salvar</button>
+    <div class="form-actions clearfix">
+        <div class="pull-left">
+            <button type="submit" class="btn btn-success btn-sm">
+                <i class="fa fa-send fa-fw" aria-hidden="true"></i>
+                Salvar
+            </button>
         </div>
-
+        <div class="pull-right">
+            <div class="col-md-12 text-right text-muted">
+                (*) Campos Obrigatórios
+            </div>
+        </div>
     </div>
 </form>
 
