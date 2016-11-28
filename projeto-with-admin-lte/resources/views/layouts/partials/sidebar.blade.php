497274809f6b12c9bcb68235e162726641c26446
Vinicius Sarmento <vinicius.eng.comp92@gmail.com>
2016-11-28 14:24:56 -0200

    Ajuste menu

--- projeto-with-admin-lte/resources/views/layouts/partials/sidebar.blade.php
+++ projeto-with-admin-lte/resources/views/layouts/partials/sidebar.blade.php
@@ -23,7 +23,9 @@
             <div class="input-group">
                 <input type="text" name="q" class="form-control" placeholder="{{ trans('adminlte_lang::message.search') }}..."/>
               <span class="input-group-btn">
-                <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i></button>
+                <button type='submit' name='search' id='search-btn' class="btn btn-flat">
+                    <i class="fa fa-search"></i>
+                </button>
               </span>
             </div>
         </form>
